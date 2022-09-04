44dea0eae40006612b119b1efb7b63ffcaf9f659
AKq <hnoofk14@gmail.com>
2022-09-04 05:51:18 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+
+    extend: {
+      colors:{
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#f7f7f7',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      }
+    },
+    fontFamily:{
+    Poppins: ["Poppins,sans-serif"],
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
